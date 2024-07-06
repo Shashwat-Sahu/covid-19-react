8cab5c82e6c619be9b497ea9fb6dbc1390f47a9a
shashwat sahu <shaswatsahu25@gmail.com>
2024-07-06 15:33:09 +0530

    build folder

---
+++ build/precache-manifest.bae24031569ab46e90bf10ecb7f1c411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941ba78afe60b255edaf029c15769cf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12e73bf842092e6e1bb5",
+    "url": "/static/css/main.edd9dcbe.chunk.css"
+  },
+  {
+    "revision": "14e7042868570f9a7628",
+    "url": "/static/js/2.7f212bcb.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.7f212bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e73bf842092e6e1bb5",
+    "url": "/static/js/main.c20db21b.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  }
+]);
